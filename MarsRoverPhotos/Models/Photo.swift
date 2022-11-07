31423507ddc0b64872e322af9a8f4dfffce970ba
GubinYuriy <iosdevelop.2022@gmail.com>
2022-11-07 17:44:17 +0500

    Add model

---
+++ MarsRoverPhotos/Models/Photo.swift
@@ -0,0 +1,34 @@
+//
+//  Photo.swift
+//  MarsRoverPhotos
+//
+//  Created by Юрий Губин on 07.11.2022.
+//
+
+struct Photo: Decodable {
+    let photos: [PhotoElement]
+}
+
+struct PhotoElement: Decodable {
+    let id: Int
+    let sol: Int
+    let camera: Camera
+    let img_src: String
+    let earth_date: String
+    let rover: Rover
+}
+
+struct Camera: Decodable {
+    let id: Int
+    let name: String
+    let rover_id: Int
+    let full_name: String
+}
+
+struct Rover: Decodable {
+    let id: Int
+    let name: String
+    let landing_date: String
+    let launch_date: String
+    let status: String
+}

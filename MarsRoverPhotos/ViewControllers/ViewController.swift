593961e8da61cb0d86f85836a47ef3aaadc092fe
GubinYuriy <iosdevelop.2022@gmail.com>
2022-11-07 17:53:27 +0500

    Add new API

--- MarsRoverPhotos/ViewControllers/ViewController.swift
+++ MarsRoverPhotos/ViewControllers/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     enum Link: String {
-        case url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=DEMO_KEY"
+        case url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=DEMO_KEY"
     }
 
     override func viewDidLoad() {
